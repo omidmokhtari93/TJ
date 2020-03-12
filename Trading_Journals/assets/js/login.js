3343e3d957189603389a807f87fdf7c4a5878651
IT2 <IT2@IT2>
2020-03-12 10:11:22 +0330

    changes in ui

--- Trading_Journals/assets/js/login.js
+++ Trading_Journals/assets/js/login.js
@@ -1,29 +1,34 @@
-﻿$('#btnLogin').on('click', function () {
-    AjaxCall({
-        url: 'api.asmx/Login',
-        param: {
-            username: $('#username').val(),
-            password: $('#password').val()
-        },
-        func: (e) => {
-            if (e.d != "false") {
-                $('body').empty().append(e.d);
-            } else {
-                $(this).notify('نام کاربری یا رمز عبور اشتباه است', { position: "bottom center", className: "error" });
-            }
-        }
-    });
-    
+﻿const Login = () => {
+  if ($('#username').val() == "" || $('#password').val() == "") return;
+  $('.login-loading').removeAttr('style');
+  AjaxCall({
+    url: 'api.asmx/Login',
+    param: {
+      username: $('#username').val(),
+      password: $('#password').val()
+    },
+    func: (e) => {
+      if (e.d != "false") {
+        $('body').empty().append(e.d);
+      } else {
+        $(this).notify('نام کاربری یا رمز عبور اشتباه است', { position: "bottom center", className: "error" });
+      }
+    }
+  });
+}
+
+$('#login').on('keydown', function (event) {
+  event.keyCode === 13 && Login();
 });
 
-$(function() {
-    AjaxCall({
-        url: 'api.asmx/CheckUserAuthenticated',
-        param: {},
-        func: (e) => {
-            if (e.d != "false") {
-                $('body').empty().append(e.d);
-            }
-        }
-    });
+$(function () {
+  AjaxCall({
+    url: 'api.asmx/CheckUserAuthenticated',
+    param: {},
+    func: (e) => {
+      if (e.d != "false") {
+        $('body').empty().append(e.d);
+      }
+    }
+  });
 });
